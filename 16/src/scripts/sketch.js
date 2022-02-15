8933160598341f3628975fed22c8798d2b60fa46
scorpiyogi <sohani.codes@gmail.com>
2022-02-15 15:54:31 +0530

    .

---
+++ 16/src/scripts/sketch.js
@@ -0,0 +1,79 @@
+let data = {
+  sketch: {
+    background: "#000000"
+  },
+  grid: {
+    ny: 350,
+    nx: 60,
+    strokeWeight: 3
+  }
+}
+
+let song
+let fft
+let loading = true
+
+function setup() {
+  let c = createCanvas(windowWidth, windowHeight)
+  console.log("Song Credits : Jeffrey Bezos By Bo Burnham")
+  song = loadSound("33.mp3", loaded)
+  fft = new p5.FFT(0.7, 512)
+}
+
+function loaded() {
+  loading = false
+  song.play()
+}
+
+function draw() {
+  background(data.sketch.background)
+
+  if (loading) {
+    push()
+    translate(width * 0.1, height * 0.1)
+    textSize(20)
+    noStroke()
+    fill(255)
+    textAlign(CENTER, CENTER)
+    text("Loading...", 0, 0)
+    pop()
+  } else {
+    let spectrum = fft.analyze()
+    push()
+    translate(width * 0.5, height * 0.5)
+    colorMode(HSB, 50)
+    noStroke()
+    rectMode(CENTER)
+    fill(255, 0, 0)
+    for (var i = 0, upperLimit_i = spectrum.length; i < upperLimit_i; i += 1) {
+      fill(map(i * 5, 0, upperLimit_i * 5, 0, 100), map(i, 0, upperLimit_i, 100, 0), map(i, 0, upperLimit_i, 100, 0))
+
+      let h = map(spectrum[i], 0, 255, 0, height * 0.5)
+      let x = map((i * width) / data.grid.ny, 0, width, -width * 0.5, width * 0.5)
+      let w = width / data.grid.ny
+      rect(x - w / 2, 0, w, h)
+    }
+    pop()
+
+    push()
+    translate(width * 0.5, height * 0.5)
+    strokeWeight(data.grid.strokeWeight)
+    for (var i = -width * 0.5, upperLimit_i = width * 0.5; i < upperLimit_i; i += width / data.grid.ny) {
+      line(i, -height * 0.5, i, height * 0.5)
+    }
+  }
+}
+
+function mousePressed() {
+  if (!loading) {
+    if (song.isPlaying()) {
+      song.pause()
+    } else {
+      song.play()
+    }
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
